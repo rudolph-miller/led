e8266e0b67afb3c2ff4ffdd89aca86eafc5fe840
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-11-24 10:47:17 +0900

    Use led.window::*window*

--- src/window.lisp
+++ src/window.lisp
@@ -1,8 +1,19 @@
 (in-package :cl-user)
 (defpackage :led.window
-  (:use :cl
-        :charms))
+  (:use :cl)
+  (:import-from :charms
+                :initialize
+                :standard-window
+                :disable-echoing
+                :enable-raw-input
+                :enable-non-blocking-mode
+                :window-dimensions
+                :write-string-at-point
+                :move-cursor
+                :refresh-window))
 (in-package :led.window)
+
+(defparameter *window* nil)
 
 (defclass window ()
   ((width :accessor window-width
@@ -22,13 +33,20 @@
 (defstruct window-line
   (content ""))
 
+(defun set-line-content (window index content)
+  (assert (<= (length content) (window-width window)))
+  (let ((line (elt (window-lines window) index)))
+    (setf (window-line-content line) content)))
+
 (defun initialize-window-lines (window)
-  (let ((lines (make-array (window-height window) :initial-element (make-window-line))))
+  (let ((lines (loop repeat (window-height window)
+                     collecting (make-window-line) into result
+                     finally (return (apply #'vector result)))))
     (setf (window-lines window) lines)))
 
 (defun initialize-window-dimensions (window)
   (assert (typep window 'curses-window))
-  (multiple-value-bind (width height) (window-dimensions *standard-window*)
+  (multiple-value-bind (width height) (window-dimensions *window*)
     (setf (window-width window) width
           (window-height window) height)))
 
@@ -38,9 +56,11 @@
 
 (defmethod initialize-instance :after ((window curses-window) &rest initargs)
   (declare (ignore initargs))
+  (initialize)
+  (setq *window* (standard-window))
   (disable-echoing)
   (enable-raw-input :interpret-control-characters t)
-  (enable-non-blocking-mode *standard-window*)
+  (enable-non-blocking-mode *window*)
   (initialize-window-dimensions window)
   (initialize-window-lines window))
 
@@ -54,8 +74,9 @@
 (defmethod refresh ((window curses-window))
   (iterate-window-lines window
                         #'(lambda (content index)
-                            (write-string-at-point *standard-window* content 0 index)))
-  (refresh-window *standard-window*))
+                            (write-string-at-point *window* content 0 index)))
+  (move-cursor *window* (window-x window) (window-y window))
+  (refresh-window *window*))
 
 (defmethod refresh ((window debug-window))
   (iterate-window-lines window
