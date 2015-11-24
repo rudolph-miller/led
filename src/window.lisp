1c605445c6a1828bb8cf35937a5cf7f546c94fd7
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-11-24 10:18:02 +0900

    Add window-line

--- src/window.lisp
+++ src/window.lisp
@@ -13,23 +13,52 @@
       :initform 0)
    (y :accessor window-y
       :initform 0)
-   (contents :accessor window-contents
-             :initform "")))
+   (lines :accessor window-lines
+          :type vector)))
 
 (defclass curses-window (window) ())
 (defclass debug-window (window) ())
+
+(defstruct window-line
+  (content ""))
+
+(defun initialize-window-lines (window)
+  (let ((lines (make-array (window-height window) :initial-element (make-window-line))))
+    (setf (window-lines window) lines)))
+
+(defun initialize-window-dimensions (window)
+  (assert (typep window 'curses-window))
+  (multiple-value-bind (width height) (window-dimensions *standard-window*)
+    (setf (window-width window) width
+          (window-height window) height)))
+
+(defmethod initialize-instance :after ((window debug-window) &rest initargs)
+  (declare (ignore initargs))
+  (initialize-window-lines window))
 
 (defmethod initialize-instance :after ((window curses-window) &rest initargs)
   (declare (ignore initargs))
   (disable-echoing)
   (enable-raw-input :interpret-control-characters t)
-  (enable-non-blocking-mode *standard-window*))
+  (enable-non-blocking-mode *standard-window*)
+  (initialize-window-dimensions window)
+  (initialize-window-lines window))
+
+(defun iterate-window-lines (window function)
+  (loop for line across (window-lines window)
+        for i from 0
+        do (funcall function (window-line-content line) i)))
 
 (defgeneric refresh (window))
 
 (defmethod refresh ((window curses-window))
-  (write-string-at-point *standard-window* (window-contents window) 0 0)
+  (iterate-window-lines window
+                        #'(lambda (content index)
+                            (write-string-at-point *standard-window* content 0 index)))
   (refresh-window *standard-window*))
 
 (defmethod refresh ((window debug-window))
-  (print (window-contents window)))
+  (iterate-window-lines window
+                        #'(lambda (content index)
+                            (declare (ignore index))
+                            (print content))))
