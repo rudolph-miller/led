d2528a4b7d77017ad7ca631de1d06aa1d135ec01
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-11-26 12:22:17 +0900

    Add make-file

--- src/file.lisp
+++ src/file.lisp
@@ -1,4 +1,18 @@
 (in-package :cl-user)
 (defpackage led.file
-  (:use :cl))
+  (:use :cl)
+  (:import-from :led.string
+                :string-to-lines))
 (in-package :led.file)
+
+(defstruct (file (:constructor %make-file))
+  lines
+  path)
+
+(defun read-file-lines (file)
+  (string-to-lines (uiop:read-file-string (file-path file))))
+
+(defun make-file (path)
+  (let ((file (%make-file :path path)))
+    (setf (file-lines file) (read-file-lines file))
+    file))
